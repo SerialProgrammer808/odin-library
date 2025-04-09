e16cc52c56f6c08098774b1d3c8abf60058e7e3e
Noah Asano <pili.asano@gmail.com>
2025-04-09 13:04:38 -1000

    Add buttons for remove and update status
    Each book needs the option to update its status (read or not read) as well as the option to remove the book from the library. This commit adds the buttons to the display as well as the javascript that makes the buttons functional

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*code for adding a book to array, no DOM manipulation yet*/
+//object constructor for book
 function Book(title, author, genre, read) {
     this.title = title;
     this.author = author;
@@ -6,24 +6,74 @@ function Book(title, author, genre, read) {
     this.read = read;
     this.id = crypto.randomUUID();
 }
-
+//library array
 const myLibrary = [];
 
+//function adds books to library array
 function addBookToLibrary(title, author, genre, read) {
   let newBook = new Book(title, author, genre, read);
   myLibrary.push(newBook);
 }
 
+//default books
 addBookToLibrary("Lord of the Flies", "William Golding", "fiction", false);
 addBookToLibrary("Harry Potter", "JK Rowling", "fiction", false);
 addBookToLibrary("Pride & Prejudice", "Jane Austen", "fiction", false);
 
-let books = document.querySelectorAll(".book");
+//adds default books
+let shelf = document.querySelector(".shelf");
 for (let bookNumber = 0; bookNumber < myLibrary.length; bookNumber++) {
-    books[bookNumber].innerHTML = `
-    <p>Title: ${myLibrary[bookNumber].title}</p>
-    <p>Author: ${myLibrary[bookNumber].author}</p>
-    <p>Genre: ${myLibrary[bookNumber].genre}</p>
-   <p> Read: ${myLibrary[bookNumber].read}</p>
-    `
+    shelf.insertAdjacentHTML("beforeend", 
+      `<div class="book-box" data-id="${myLibrary[bookNumber].id}">
+        <div class="book">
+          <p>Title: ${myLibrary[bookNumber].title}</p>
+          <p>Author: ${myLibrary[bookNumber].author}</p>
+          <p>Genre: ${myLibrary[bookNumber].genre}</p>
+          <p class="status"> Read: ${myLibrary[bookNumber].read}</p>
+        </div>
+        <div class="actions">
+          <div class="remove">Remove</div>
+          <div class="change-status">Change Status</div>
+        </div>
+      </div>`); 
+}
+
+//logic for removing and changing status
+let removeButtons = document.querySelectorAll(".remove");
+let statusButtons = document.querySelectorAll(".change-status");
+
+//buttons for dynamicremoval
+removeButtons.forEach((button) => {
+  button.addEventListener("click", (button) => {
+    removeBook(button);});
+  button.addEventListener("mouseenter", () => {
+    button.style.filter = "brightness(80%)"});
+  button.addEventListener("mouseleave", () => {
+    button.style.filter = "brightness(100%)"});
+});
+
+//buttons for changing status
+statusButtons.forEach((button) => {
+  button.addEventListener("click", (button) => {
+    changeStatus(button);});
+  button.addEventListener("mouseenter", () => {
+    button.style.filter = "brightness(80%)"});
+  button.addEventListener("mouseleave", () => {
+    button.style.filter = "brightness(100%)"});
+});
+
+function removeBook(button) {
+  let bookBox = button.target.parentElement.parentElement;
+  bookBox.remove();
+}
+
+function changeStatus(button) {
+  let bookBox = button.target.parentElement.parentElement;
+  let status = bookBox.querySelector(".status");
+  let bookId = bookBox.getAttribute("data-id");
+  let book = myLibrary.find(b => b.id === bookId);
+
+  book.read = !book.read
+
+  status.innerHTML = `<p class="status"> Read: ${book.read} </p>`;
 }
